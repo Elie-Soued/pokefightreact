c3b1912df4a8b78f3904d72d1295ce31f1a7fb0f
Hermann Marx <marxhermann@web.de>
2021-02-11 16:00:09 +0100

    added ViewPokemonInfo component with API request

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import { Button } from "@material-ui/core";
+import ViewPokemonInfo from "./views/ViewPokemonInfo";
+
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path="/pokemon/fight">
-          <ViewPokeFight />
-        </Route>
+        <Route path="/pokemon/fight">{/* <ViewPokeFight /> */}</Route>
         <Route path="/pokemon/:id/:info">
           <ViewPokemonInfo />
         </Route>
-        <Route path="/pokemon/:id">
-          <ViewPokemon />
-        </Route>
-        <Route path="/pokemon">
-          <ViewPokemonAll />
-        </Route>
+        <Route path="/pokemon/:id">{/*   <ViewPokemon /> */}</Route>
+        <Route path="/pokemon">{/*  <ViewPokemonAll /> */}</Route>
       </Switch>
     </div>
   );
--- src/views/ViewPokemonInfo.js
+++ src/views/ViewPokemonInfo.js
@@ -1,3 +1,50 @@
-const ViewPokemonInfo = ({}) => {};
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+const ViewPokemonInfo = ({}) => {
+  const { id, info } = useParams();
+  // const { pokeArray, setPokeArray } = useParams([]);
+  let [pokeInfo, setPokeInfo] = useState(null);
+  let [pokeInfo2, setPokeInfo2] = useState(null);
+
+  const getPokeInfo = async () => {
+    await axios
+      .get(`http://localhost:3000/pokemon/${id}/${info}`)
+      .then((res) => {
+        let pokeLength = Object.values(res.data).length;
+        let pokeValues = [];
+        let pokeKeys = [];
+        for (let i = 0; i < pokeLength; i++) {
+          pokeValues.push(Object.values(res.data)[i]);
+        }
+        setPokeInfo(pokeValues);
+
+        for (let i = 0; i < pokeLength; i++) {
+          pokeKeys.push(Object.keys(res.data)[i]);
+        }
+        setPokeInfo2(pokeKeys);
+      })
+      .catch((e) => console.log(e));
+  };
+
+  useEffect(() => getPokeInfo(), []);
+
+  return (
+    <div>
+      <h1>{info}</h1>
+
+      {pokeInfo !== null && pokeInfo2 !== null ? (
+        pokeInfo.map((iteration, index) => (
+          <p>
+            {pokeInfo2[index]}: {iteration}
+          </p>
+        ))
+      ) : (
+        <p>Still loading?</p>
+      )}
+    </div>
+  );
+};
 
 export default ViewPokemonInfo;
